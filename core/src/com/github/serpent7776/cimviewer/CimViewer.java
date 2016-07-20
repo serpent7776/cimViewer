02d50c710c7d7239dd8fcdfedd820362d966c299
serpent7776 <serpent7776@gmail.com>
2016-07-20 17:51:34 +0200

    view cim files given as commandline argument

--- core/src/com/github/serpent7776/cimviewer/CimViewer.java
+++ core/src/com/github/serpent7776/cimviewer/CimViewer.java
@@ -2,32 +2,45 @@ package com.github.serpent7776.cimviewer;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.PixmapIO;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class CimViewer extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+	private final FileHandle handle;
+	private SpriteBatch batch;
+	private Pixmap pixmap;
+	private Texture img;
+
+	public CimViewer(FileHandle handle) {
+		this.handle = handle;
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	public void create() {
+		batch = new SpriteBatch();
+		pixmap = PixmapIO.readCIM(handle);
+		img = new Texture(pixmap);
+	}
+
+	@Override
+	public void render() {
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
 	}
-	
+
 	@Override
-	public void dispose () {
+	public void dispose() {
 		batch.dispose();
 		img.dispose();
+		pixmap.dispose();
 	}
+
 }
--- desktop/src/com/github/serpent7776/cimviewer/desktop/DesktopLauncher.java
+++ desktop/src/com/github/serpent7776/cimviewer/desktop/DesktopLauncher.java
@@ -2,11 +2,16 @@ package com.github.serpent7776.cimviewer.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.files.FileHandle;
+
 import com.github.serpent7776.cimviewer.CimViewer;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+
+	public static void main(String[] args) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new CimViewer(), config);
+		final FileHandle handle = args.length > 0 ? new FileHandle(args[0]) : null;
+		new LwjglApplication(new CimViewer(handle), config);
 	}
+
 }
