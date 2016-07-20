b441edaf09215283033a63230b14d8f944313497
serpent7776 <serpent7776@gmail.com>
2016-07-20 17:34:03 +0200

    initial commit

---
+++ desktop/src/com/github/serpent7776/cimviewer/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.github.serpent7776.cimviewer.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.serpent7776.cimviewer.CimViewer;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CimViewer(), config);
+	}
+}
