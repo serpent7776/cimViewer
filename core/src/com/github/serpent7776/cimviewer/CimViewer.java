563d943c143cf9cc2bd6846d80da3b7a44ff34f5
serpent7776 <serpent7776@gmail.com>
2016-07-21 11:20:18 +0200

    always scale image to fit window

--- core/src/com/github/serpent7776/cimviewer/CimViewer.java
+++ core/src/com/github/serpent7776/cimviewer/CimViewer.java
@@ -8,16 +8,22 @@ import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.PixmapIO;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Scaling;
 
 public class CimViewer extends ApplicationAdapter {
 
 	private final FileHandle handle;
+	private final Vector2 size;
+	private final Vector2 offset;
 	private SpriteBatch batch;
 	private Pixmap pixmap;
 	private Texture img;
 
 	public CimViewer(FileHandle handle) {
 		this.handle = handle;
+		this.size = new Vector2();
+		this.offset = new Vector2();
 	}
 
 	@Override
@@ -31,13 +37,16 @@ public class CimViewer extends ApplicationAdapter {
 	@Override
 	public void resize(int width, int height) {
 		batch.getProjectionMatrix().setToOrtho2D(0, 0, width, height);
+		final Vector2 vec = Scaling.fit.apply(img.getWidth(), img.getHeight(), width, height);
+		size.set(vec);
+		offset.set((width - size.x) / 2, (height - size.y) / 2);
 	}
 
 	@Override
 	public void render() {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, offset.x, offset.y, size.x, size.y);
 		batch.end();
 	}
 
